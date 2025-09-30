1b622644ef885964591fb5cf28719e2cce374de1
Justin Bayer <justin.bayer@foundation.bot>
2025-09-30 15:58:12 +0200

    Extend carry and add numerical safety.

--- src/seher/stepper/mppi.py
+++ src/seher/stepper/mppi.py
@@ -16,6 +16,8 @@ from ..types import (
     StepperCarry,
 )
 
+HUGE_COST = 1e16
+
 
 @dataclass
 class GaussianMPPIOptimizerCarry(StepperCarry[jax.Array]):
@@ -28,11 +30,16 @@ class GaussianMPPIOptimizerCarry(StepperCarry[jax.Array]):
         search distribution.
     scale:
         The scale parameter of the last plan.
-
+    candidates:
+        All candidates from the last planning step.
+    candidate_costs:
+        Costs of candidates at the last step.
     """
 
     current: jax.Array
     scale: jax.Array
+    candidates: jax.Array
+    candidate_costs: jax.Array
 
 
 @dataclass
@@ -90,7 +97,16 @@ class GaussianMPPIOptimizer[ProblemData](
         initial_scale = jnp.zeros_like(sample_parameter) + self.initial_scale
 
         return GaussianMPPIOptimizerCarry(
-            current=initial_loc, scale=initial_scale
+            current=initial_loc,
+            scale=initial_scale,
+            candidates=jnp.zeros(
+                (
+                    self.n_candidates,
+                    sample_parameter.shape[0],
+                    sample_parameter.shape[1],
+                )
+            ),
+            candidate_costs=jnp.zeros(self.n_candidates),
         )
 
     initial_carry.__doc__ = Stepper.initial_carry.__doc__
@@ -120,6 +136,13 @@ class GaussianMPPIOptimizer[ProblemData](
         get_costs = jax.vmap(self.objective, in_axes=(0, None, None))
         total_costs, _ = get_costs(candidates, problem_data, eval_key)
 
+        # Some candidates might run into infeasible solutions, which are shown by
+        # their cost being not finite. To not hurt optimisation, we overwrite
+        # them with a huge number.
+        total_costs = jnp.where(
+            jnp.isfinite(total_costs), total_costs, HUGE_COST
+        )
+
         # Pick the best k.
         _, best_idxs = jl.top_k(-total_costs, k=self.top_k)
         best = candidates[best_idxs]
@@ -133,6 +156,15 @@ class GaussianMPPIOptimizer[ProblemData](
         scale = jnp.maximum(scale, self.min_scale)
         scale = jnp.minimum(scale, self.max_scale)
 
-        return GaussianMPPIOptimizerCarry(current=loc, scale=scale), loc, None
+        return (
+            GaussianMPPIOptimizerCarry(
+                current=loc,
+                scale=scale,
+                candidates=candidates,
+                candidate_costs=total_costs,
+            ),
+            loc,
+            None,
+        )
 
     __call__.__doc__ = Stepper.__call__.__doc__
